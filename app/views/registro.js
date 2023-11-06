1d20070468260381d16a3f8a743d85b62e1429bb
eduardo1403 <eduardo1403hm@gmail.com>
2023-11-06 04:16:07 -0600

    feat - Registro de usuario
    Se realiza la conexión a la API y se realiza el registro de un usuario, la base de datos actualiza en tiempo real

--- app/views/registro.js
+++ app/views/registro.js
@@ -1,44 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button, View, Image,TextInput, Text, Pressable} from 'react-native';
 import {styles} from '../styles/login'
 import { useNavigation } from "@react-navigation/native";
+import axios from 'axios';
 
 const registro = () =>  {
-  const [usuario, setUsuario] = useState('');
-  const [contrasena, setContrasena] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [isValidEmail, setIsValidEmail] = useState(true);
   const [isValidPassword, setIsValidPassword] = useState(true);
-  const [nombre, setNombre] = useState('');
+  const [name, setName] = useState('');
   const [isValidNombre, setIsValidNombre] = useState(true);
   const [confirmarContrasena, setConfirmarContrasena] = useState('');
   const [passwordsMatch, setPasswordsMatch] = useState(true);
-  const [apellidos, setApellidos] = useState('');
+  const [lastName, setLastName] = useState('');
   const [isValidApellidos, setIsValidApellidos] = useState(true);
+  const [usuarios, setUsuarios] = useState([]);
+
+
+  const registrarUser = () =>{
+    const users = {
+      name,
+      lastName,
+      email,
+      password,
+    };
+    const url = 'https://api-backend-mqv1.onrender.com/api/users';
+
+    axios
+      .post(url, users)
+      .then(response => {
+        // El usuario se agregó correctamente
+        alert('El usuario se agregó correctamente')
+      })
+      .catch(error => {
+        // Se produjo un error al agregar el usuario
+        alert('Se produjo un error al agregar el usuario')
+      });
+  };
+
+  useEffect(() => {
+    setName('');
+    setLastName('');
+    setEmail('');
+    setPassword('');
+  },[]);
 
   const validateNombre = () => {
     const nombreRegex = /^[A-Za-z]+$/;
-    const isValid = nombreRegex.test(nombre);
+    const isValid = nombreRegex.test(name);
     setIsValidNombre(isValid);
   };
 
   const validateApellidos = () => {
     const apellidosRegex = /^[A-Za-z]+$/;
-    const isValid = apellidosRegex.test(apellidos);
+    const isValid = apellidosRegex.test(lastName);
     setIsValidApellidos(isValid);
   };
 
   const validateEmail = () => {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
-    const isValid = emailRegex.test(usuario);
+    const isValid = emailRegex.test(email);
     setIsValidEmail(isValid);
   };
 
   const validatePassword = () => {
-    setIsValidPassword(contrasena.length >= 6);
+    setIsValidPassword(password.length >= 6);
   };
 
   const validateConfirmPassword = () => {
-    setPasswordsMatch(contrasena === confirmarContrasena);
+    setPasswordsMatch(password === confirmarContrasena);
 };
   const navigation = useNavigation();
   return (
@@ -59,24 +90,24 @@ const registro = () =>  {
           <TextInput
         style={styles.input}
         placeholder="Nombre"
-        value={nombre}
-        onChangeText={(text) => setNombre(text)}
+        value={name}
+        onChangeText={(text) => setName(text)}
         onBlur={validateNombre}
       />
       {!isValidNombre && <Text style={{ color: 'red' }}>El nombre no debe contener números o caracteres especiales</Text>}
       <TextInput
         style={styles.input}
         placeholder="Apellidos"
-        value={apellidos}
-        onChangeText={(text) => setApellidos(text)}
+        value={lastName}
+        onChangeText={(text) => setLastName(text)}
         onBlur={validateApellidos}
       />
       {!isValidApellidos && <Text style={{ color: 'red' }}>Los apellidos no deben contener números o caracteres especiales</Text>}
           <TextInput
         style={styles.input}
         placeholder="Correo electrónico"
-        value={usuario}
-        onChangeText={(text) => setUsuario(text)}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
         onBlur={validateEmail}
       />
       {!isValidEmail && <Text style={{ color: 'red' }}>Correo electrónico no válido</Text>}
@@ -84,8 +115,8 @@ const registro = () =>  {
         style={styles.input}
         placeholder="Contraseña"
         secureTextEntry={true}
-        value={contrasena}
-        onChangeText={(text) => setContrasena(text)}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
         onBlur={validatePassword}
       />
       {!isValidPassword && <Text style={{ color: 'red' }}>Contraseña no válida</Text>}
@@ -101,8 +132,9 @@ const registro = () =>  {
 
           <View style={styles.buttonContainer}>
               <Button
-              title="Iniciar sesión"
+              title="Registrarme"
               color="#DC3545"
+              onPress={registrarUser}
               />
           </View>
       </View>
